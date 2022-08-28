bccab529f70dd09c954db3e9f44f7714c25a9a0c
mr7aali <mr7aali@gmail.com>
2022-08-28 23:27:29 +0600

    country api example

--- Api Example/js/randomUser.js
+++ Api Example/js/randomUser.js
@@ -1,26 +1,51 @@
 const loadUsers = ()=>{
-        fetch('https://randomuser.me/api/?results=20')
+        fetch('https://restcountries.com/v3.1/all')
         .then(res => res.json())
-        .then(data => displayUsers(data.results))
+        .then(data => displayUsers(data))
 }
-const displayUsers = users => {
+const displayUsers = countries => {
         
 
         // console.log(users);
           const userDiv = document.getElementById('users-container');
   
 
-        for(const user of users){
-                console.log(user);
+        for(const country of countries){
+                // console.log(country);
                 const newDiv = document.createElement('div');
                 newDiv.classList.add('user')
+
+
                 newDiv.innerHTML =`
-                  <h1> Username : ${user.name.first} ${user.name.last} </h1>
-                  <p> Emain :${user.email}</P>
+                  <h1> Name :${country.name.common} </h1>
+                  <p>Capital  : ${country.capital ? country.capital[0] : 'No Capital'} </P>
+                  <p>Capital  : ${country.capital} </P>
                   
-                  <p> User Location ${ user.location.country} </P>
+                 
+                  <button onclick="loadCountryDetail('${country.cca2}')"> Details </button>
                             `;
                 userDiv.appendChild(newDiv);
         }     
 }
+
+loadCountryDetail = (code)=>{
+
+        const url =`https://restcountries.com/v3.1/alpha/${code}`
+        // console.log('country details',code);
+
+        fetch(url)
+        .then(res => res.json())
+        .then(data => displayCountryDetails(data[0]))
+}
+
+displayCountryDetails= country=>{
+        console.log(country)
+        const countryDetail = document.getElementById('country-details');
+        countryDetail.innerHTML=`
+        <h1> Name :${country.name.common} </h1>
+        <img src="${country.flags.png}" alt="">
+        `;
+
+}
+
 loadUsers ();
